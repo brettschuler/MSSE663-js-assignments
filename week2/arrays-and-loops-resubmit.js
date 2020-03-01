52ec173dec1201476abf328dc8319382cee4ca73
brettschuler <56987307+brettschuler@users.noreply.github.com>
2020-03-01 00:36:00 -0600

    Arrays and Loops Resubmit -- Brett Schuler
    Arrays and Loops Resubmit -- Brett Schuler

---
+++ week2/arrays-and-loops-resubmit.js
@@ -0,0 +1,53 @@
+// #1 Take an array of numbers and make them strings
+
+var arr = [2, 5, 100];
+
+function stringItUp(arr) {
+	// your code here
+	return arr.map(element,number) => stringItUp(element);
+};
+
+
+console.log(stringItUp([2, 5, 100]));
+// output: ["2", "5", "100"]
+
+// #2 Make an array of strings of the names
+function namesOnly(arr) {
+	// your code here
+	return arr.filter((person) => person.name);
+};
+
+console.log(
+	namesOnly([
+		{
+			name: 'Angelina Jolie',
+			age: 80,
+		},
+		{
+			name: 'Eric Jones',
+			age: 2,
+		},
+		{
+			name: 'Paris Hilton',
+			age: 5,
+		},
+		{
+			name: 'Kayne West',
+			age: 16,
+		},
+		{
+			name: 'Bob Ziroll',
+			age: 100,
+		},
+	])
+);
+// output: ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+
+// #3 Filter the output array from #2.
+
+
+function oldOnesOnly(arr) {
+	// your code here
+
+    return arr.filter(person => person.age>20).map(person=>person.age)};
+// output: [80, 100]
